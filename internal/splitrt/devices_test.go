f5ea10f9b9d77f8e6da471d833bb2afc016e04b9
hwipl <33433250+hwipl@users.noreply.github.com>
2022-09-14 17:24:16 +0200

    Add tests for Devices in splitrt
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/splitrt/devices_test.go
@@ -0,0 +1,111 @@
+package splitrt
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/T-Systems-MMS/oc-daemon/internal/devmon"
+)
+
+// getTestDevMonUpdate returns a DevMon Update for testing
+func getTestDevMonUpdate() *devmon.Update {
+	return &devmon.Update{
+		Add:    true,
+		Device: "tun0",
+		Type:   "device",
+		Index:  1,
+	}
+}
+
+// TestDevicesAdd tests Add of Devices
+func TestDevicesAdd(t *testing.T) {
+	d := NewDevices()
+	update := getTestDevMonUpdate()
+	d.Add(update)
+	want := update
+	got := d.m[update.Index]
+	if got != want {
+		t.Errorf("got %p, want %p", got, want)
+	}
+}
+
+// TestDevicesRemove tests Remove of Devices
+func TestDevicesRemove(t *testing.T) {
+	d := NewDevices()
+	update := getTestDevMonUpdate()
+
+	// add device
+	d.Add(update)
+	want := update
+	got := d.m[update.Index]
+	if got != want {
+		t.Errorf("got %p, want %p", got, want)
+	}
+
+	// test removing device
+	d.Remove(update)
+	want = nil
+	got = d.m[update.Index]
+	if got != want {
+		t.Errorf("got %p, want %p", got, want)
+	}
+}
+
+// TestDevicesGetReal tests GetReal of Devices
+func TestDevicesGetReal(t *testing.T) {
+	d := NewDevices()
+	realDev := getTestDevMonUpdate()
+	virtDev := getTestDevMonUpdate()
+	virtDev.Type = "virtual"
+	virtDev.Index = 2
+	d.Add(realDev)
+	d.Add(virtDev)
+
+	want := []int{realDev.Index}
+	got := d.GetReal()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+// TestDevicesGetVirtual tests GetVirtual of Devices
+func TestDevicesGetVirtual(t *testing.T) {
+	d := NewDevices()
+	realDev := getTestDevMonUpdate()
+	virtDev := getTestDevMonUpdate()
+	virtDev.Type = "virtual"
+	virtDev.Index = 2
+	d.Add(realDev)
+	d.Add(virtDev)
+
+	want := []int{virtDev.Index}
+	got := d.GetVirtual()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+// TestDevicesGetAll tests GetAll of Devices
+func TestDevicesGetAll(t *testing.T) {
+	d := NewDevices()
+	realDev := getTestDevMonUpdate()
+	virtDev := getTestDevMonUpdate()
+	virtDev.Type = "virtual"
+	virtDev.Index = 2
+	d.Add(realDev)
+	d.Add(virtDev)
+
+	want := []int{realDev.Index, virtDev.Index}
+	got := d.GetAll()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+// TestNewDevices tests NewDevices
+func TestNewDevices(t *testing.T) {
+	d := NewDevices()
+	if d.m == nil {
+		t.Errorf("got nil, want != nil")
+	}
+}
