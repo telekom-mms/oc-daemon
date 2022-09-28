fc0c228fdbb1fbee84aaae1ed4d5314b4b9d2277
hwipl <33433250+hwipl@users.noreply.github.com>
2022-09-28 11:46:57 +0200

    add tests for ClientConfig in client
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/client/config_test.go
@@ -0,0 +1,74 @@
+package client
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// TestClientConfigEmpty tests empty of ClientConfig
+func TestClientConfigEmpty(t *testing.T) {
+	// test empty
+	c := newClientConfig()
+	want := true
+	got := c.empty()
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+
+	// test not empty
+	c.User = "User"
+	want = false
+	got = c.empty()
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+
+	// test nil
+	c = nil
+	want = true
+	got = c.empty()
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+}
+
+// TestLoacClientConfig tests save of ClientConfig and loadClientConfig
+func TestLoadClientConfig(t *testing.T) {
+	// create test config
+	want := &ClientConfig{
+		ClientCertificate: "/some/cert",
+		ClientKey:         "/some/key",
+		CACertificate:     "/some/ca",
+		VPNServer:         "server.example.com",
+		User:              "user1",
+	}
+
+	// create temporary file
+	f, err := os.CreateTemp("", "oc-client-config")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	// save config to temporary file
+	want.save(f.Name())
+
+	// load config from temporary file
+	got := loadClientConfig(f.Name())
+
+	// make sure configs are equal
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+// TestNewClientConfig tests newClientConfig
+func TestNewClientConfig(t *testing.T) {
+	c := newClientConfig()
+	if c == nil {
+		t.Errorf("got nil, want != nil")
+	}
+}
