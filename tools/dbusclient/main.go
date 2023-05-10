0e58496b2cb2e54090860209f863086d7f8a3f96
hwipl <33433250+hwipl@users.noreply.github.com>
2023-05-10 20:08:39 +0200

    Add tools/dbusclient
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ tools/dbusclient/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/T-Systems-MMS/oc-daemon/internal/dbusapi"
+	"github.com/godbus/dbus/v5"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	// connect to session bus
+	conn, err := dbus.ConnectSystemBus()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = conn.Close() }()
+
+	// subscribe to properties changed signals
+	if err = conn.AddMatchSignal(
+		dbus.WithMatchSender(dbusapi.Interface),
+		dbus.WithMatchInterface("org.freedesktop.DBus.Properties"),
+		dbus.WithMatchMember("PropertiesChanged"),
+		dbus.WithMatchPathNamespace(dbusapi.Path),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	// get initial values of properties
+	trustedNetwork := dbusapi.TrustedNetworkUnknown
+	connectionState := dbusapi.ConnectionStateUnknown
+	ip := dbusapi.IPInvalid
+	device := dbusapi.DeviceInvalid
+	connectedAt := dbusapi.ConnectedAtInvalid
+	servers := dbusapi.ServersInvalid
+
+	getProperty := func(name string, val any) {
+		err = conn.Object(dbusapi.Interface, dbusapi.Path).
+			StoreProperty(dbusapi.Interface+"."+name, val)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	getProperty(dbusapi.PropertyTrustedNetwork, &trustedNetwork)
+	getProperty(dbusapi.PropertyConnectionState, &connectionState)
+	getProperty(dbusapi.PropertyIP, &ip)
+	getProperty(dbusapi.PropertyDevice, &device)
+	getProperty(dbusapi.PropertyConnectedAt, &connectedAt)
+	getProperty(dbusapi.PropertyServers, &servers)
+
+	log.Println("TrustedNetwork:", trustedNetwork)
+	log.Println("ConnectionState:", connectionState)
+	log.Println("IP:", ip)
+	log.Println("Device:", device)
+	log.Println("ConnectedAt:", connectedAt)
+	log.Println("Servers:", servers)
+
+	// handle signals
+	c := make(chan *dbus.Signal, 10)
+	conn.Signal(c)
+	for s := range c {
+		// make sure it's a properties changed signal
+		if s.Path != dbusapi.Path ||
+			s.Name != "org.freedesktop.DBus.Properties.PropertiesChanged" {
+			log.Error("Not a properties changed signal")
+			continue
+		}
+
+		// check properties changed signal
+		if v, ok := s.Body[0].(string); !ok || v != dbusapi.Interface {
+			log.Error("Not the right properties changed signal")
+			continue
+		}
+
+		// get changed properties
+		changed, ok := s.Body[1].(map[string]dbus.Variant)
+		if !ok {
+			log.Error("Invalid changed properties in properties changed signal")
+			continue
+		}
+		for name, value := range changed {
+			fmt.Printf("Changed property: %s ", name)
+			switch name {
+			case dbusapi.PropertyTrustedNetwork:
+				if err := value.Store(&trustedNetwork); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(trustedNetwork)
+			case dbusapi.PropertyConnectionState:
+				if err := value.Store(&connectionState); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(connectionState)
+			case dbusapi.PropertyIP:
+				if err := value.Store(&ip); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(ip)
+			case dbusapi.PropertyDevice:
+				if err := value.Store(&device); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(device)
+			case dbusapi.PropertyConnectedAt:
+				if err := value.Store(&connectedAt); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(connectedAt)
+			case dbusapi.PropertyServers:
+				if err := value.Store(&servers); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(servers)
+			}
+		}
+
+		// get invalidated properties
+		invalid, ok := s.Body[2].([]string)
+		if !ok {
+			log.Error("Invalid invalidated properties in properties changed signal")
+		}
+		for _, name := range invalid {
+			// not expected to happen currently, but handle it anyway
+			switch name {
+			case dbusapi.PropertyTrustedNetwork:
+				trustedNetwork = dbusapi.TrustedNetworkUnknown
+			case dbusapi.PropertyConnectionState:
+				connectionState = dbusapi.ConnectionStateUnknown
+			case dbusapi.PropertyIP:
+				ip = dbusapi.IPInvalid
+			case dbusapi.PropertyDevice:
+				device = dbusapi.DeviceInvalid
+			case dbusapi.PropertyConnectedAt:
+				connectedAt = dbusapi.ConnectedAtInvalid
+			case dbusapi.PropertyServers:
+				servers = dbusapi.ServersInvalid
+			}
+			fmt.Printf("Invalidated property: %s\n", name)
+		}
+	}
+}
