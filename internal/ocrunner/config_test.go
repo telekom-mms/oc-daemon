7abffee1224903a088a5120e21b03164c48a1be7
hwipl <33433250+hwipl@users.noreply.github.com>
2023-08-15 15:49:43 +0200

    Add OpenConnect Runner Config
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/ocrunner/config_test.go
@@ -0,0 +1,77 @@
+package ocrunner
+
+import (
+	"reflect"
+	"testing"
+)
+
+// TestConfigValid tests Valid of Config
+func TestConfigValid(t *testing.T) {
+	// test invalid
+	for _, invalid := range []*Config{
+		nil,
+		{},
+		{
+			XMLProfile:     "/test/profile",
+			VPNCScript:     "/test/vpncscript",
+			VPNDevice:      "test-device",
+			PIDFile:        "/test/pid",
+			PIDPermissions: "invalid",
+		},
+		{
+			XMLProfile:     "/test/profile",
+			VPNCScript:     "/test/vpncscript",
+			VPNDevice:      "test-device",
+			PIDFile:        "/test/pid",
+			PIDPermissions: "1234",
+		},
+	} {
+		want := false
+		got := invalid.Valid()
+
+		if got != want {
+			t.Errorf("got %t, want %t for %v", got, want, invalid)
+		}
+	}
+
+	// test valid
+	for _, valid := range []*Config{
+		NewConfig(),
+		{
+			XMLProfile:     "/test/profile",
+			VPNCScript:     "/test/vpncscript",
+			VPNDevice:      "test-device",
+			PIDFile:        "/test/pid",
+			PIDPermissions: "777",
+		},
+	} {
+		want := true
+		got := valid.Valid()
+
+		if got != want {
+			t.Errorf("got %t, want %t for %v", got, want, valid)
+		}
+	}
+}
+
+// TestNewConfig tests NewConfig
+func TestNewConfig(t *testing.T) {
+	want := &Config{
+		XMLProfile: XMLProfile,
+		VPNCScript: VPNCScript,
+		VPNDevice:  VPNDevice,
+
+		PIDFile:        PIDFile,
+		PIDOwner:       PIDOwner,
+		PIDGroup:       PIDGroup,
+		PIDPermissions: PIDPermissions,
+
+		NoProxy:   NoProxy,
+		ExtraEnv:  ExtraEnv,
+		ExtraArgs: ExtraArgs,
+	}
+	got := NewConfig()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
