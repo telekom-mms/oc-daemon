f566e55dcc1a01ead519f62ecc7761c22d213756
hwipl <33433250+hwipl@users.noreply.github.com>
2023-12-20 19:51:07 +0100

    WIP: update tests in package api, server
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/api/server_test.go
+++ internal/api/server_test.go
@@ -1,6 +1,117 @@
 package api
 
-import "testing"
+import (
+	"net"
+	"reflect"
+	"testing"
+)
+
+// TestServerHandleRequest tests handleRequest of Server.
+func TestServerHandleRequest(t *testing.T) {
+	config := NewConfig()
+	server := NewServer(config)
+
+	// connection closed
+	c1, c2 := net.Pipe()
+	if err := c2.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	server.handleRequest(c1)
+
+	// invalid message
+	c1, c2 = net.Pipe()
+	go server.handleRequest(c1)
+
+	msg := &Message{Header: Header{Type: TypeUndefined}}
+	if err := WriteMessage(c2, msg); err != nil {
+		t.Fatal(err)
+	}
+
+	// unhandled message type
+	c1, c2 = net.Pipe()
+	go server.handleRequest(c1)
+
+	msg = &Message{Header: Header{Type: TypeError}}
+	if err := WriteMessage(c2, msg); err != nil {
+		t.Fatal(err)
+	}
+	if err := c2.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// valid request
+	c1, c2 = net.Pipe()
+	go server.handleRequest(c1)
+
+	msg = &Message{Header: Header{Type: TypeVPNConfigUpdate}}
+	if err := WriteMessage(c2, msg); err != nil {
+		t.Fatal(err)
+	}
+
+	req := <-server.requests
+	if !reflect.DeepEqual(req.msg.Header, msg.Header) {
+		t.Errorf("got %v, want %v", req.msg.Header, msg.Header)
+	}
+	if req.conn != c1 {
+		t.Errorf("got %p, want %p", req.conn, c1)
+	}
+}
+
+// TestServerSetSocketOwner tests setSocketOwner of Server.
+func TestServerSetSocketOwner(t *testing.T) {
+	config := NewConfig()
+	server := NewServer(config)
+
+	// no changes
+	server.setSocketOwner()
+
+	// user does not exist
+	server.config.SocketOwner = "does-not-exist"
+	server.setSocketOwner()
+
+	// socket file does not exist
+	server.config.SocketOwner = "root"
+	server.config.SocketFile = "does-not-exists"
+	server.setSocketOwner()
+}
+
+// TestServerSetSocketGroup tests setSocketGroup of Server.
+func TestServerSetSocketGroup(t *testing.T) {
+	config := NewConfig()
+	server := NewServer(config)
+
+	// no changes
+	server.setSocketGroup()
+
+	// group does not exist
+	server.config.SocketGroup = "does-not-exist"
+	server.setSocketGroup()
+
+	// socket file does not exist
+	server.config.SocketGroup = "root"
+	server.config.SocketFile = "does-not-exists"
+	server.setSocketGroup()
+}
+
+// TestServerSetSocketPermissions tests setSocketPermissions of Server.
+// TODO: can we check anything? return errors in method?
+func TestServerSetSocketPermissions(t *testing.T) {
+	config := NewConfig()
+	server := NewServer(config)
+
+	// socket file does not exist
+	server.config.SocketFile = "does-not-exists"
+	server.setSocketPermissions()
+
+	// invalid permissions
+	server.config.SocketPermissions = "invalid"
+	server.setSocketPermissions()
+
+	// no changes
+	server.config.SocketPermissions = ""
+	server.setSocketPermissions()
+}
 
 // TestServerStartStop tests Start and Stop of Server
 func TestServerStartStop(t *testing.T) {
