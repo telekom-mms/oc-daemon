90e9f09ce6993a023fbf6e7bcfc06d5c4bc5906a
hwipl <33433250+hwipl@users.noreply.github.com>
2023-12-21 18:52:33 +0100

    WIP: update CPD tests
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cpd/cpd_test.go
+++ internal/cpd/cpd_test.go
@@ -1,15 +1,116 @@
 package cpd
 
 import (
-	"log"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
+	"time"
 )
 
-// testCPDStartStop tests Start and Stop of CPD
-func TestCPDStartStop(t *testing.T) {
+func TestCPDProbeCheck(t *testing.T) {
+	ProbeWait = 0
+
+	// status code , detected, early stop
+	t.Run("stop during probe", func(_ *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+		}))
+		defer ts.Close()
+		c := NewCPD(NewConfig())
+		c.config.Host = ts.Listener.Addr().String()
+		close(c.done)
+		c.probe()
+	})
+
+	t.Run("redirect without url", func(_ *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusFound)
+		}))
+		defer ts.Close()
+		c := NewCPD(NewConfig())
+		c.config.Host = ts.Listener.Addr().String()
+		c.check()
+	})
+
+	t.Run("invalid server", func(_ *testing.T) {
+		c := NewCPD(NewConfig())
+		c.config.Host = ""
+		c.check()
+	})
+
+	t.Run("invalid content length", func(_ *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Length", "100")
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer ts.Close()
+		c := NewCPD(NewConfig())
+		c.config.Host = ts.Listener.Addr().String()
+		c.check()
+	})
+}
+
+func TestCPDHandleProbeRequest(t *testing.T) {
 	c := NewCPD(NewConfig())
+	close(c.done)
+
+	c.handleProbeRequest()
+	if !c.running {
+		t.Error("should be running")
+	}
+
+	c.running = true
+	c.handleProbeRequest()
+	if !c.running {
+		t.Error("should still be running")
+	}
+}
+
+func TestCPDHandleProbeReport(t *testing.T) {
+	c := NewCPD(NewConfig())
+
+	go func() {
+		c.probes <- struct{}{}
+		<-c.reports
+		close(c.done)
+	}()
+
+	// TODO: read and compare reports?
+	c.handleProbeReport(&Report{Detected: true})
+	if !c.detected {
+		t.Error("detected should be true")
+	}
+	if !c.running {
+		t.Error("should be running")
+	}
+	//c.running = true
+	c.handleProbeReport(&Report{})
+}
+
+func TestCPDHandleTimer(t *testing.T) {
+	for _, detected := range []bool{
+		false,
+		true,
+	} {
+		c := NewCPD(NewConfig())
+		c.timer = time.NewTimer(0)
+		c.detected = detected
+		c.handleTimer()
+
+		if c.running != true {
+			t.Error("probe should be running")
+		}
+	}
+}
+
+// TestCPDStartStop tests Start and Stop of CPD
+func TestCPDStartStop(t *testing.T) {
+	conf := NewConfig()
+	conf.ProbeTimer = 0
+	c := NewCPD(conf)
 	c.Start()
+	<-c.Results()
 	c.Stop()
 }
 
@@ -27,11 +128,41 @@ func TestCPDHosts(t *testing.T) {
 
 // TestCPDProbe tests Probe of CPD
 func TestCPDProbe(t *testing.T) {
-	c := NewCPD(NewConfig())
-	c.Start()
-	c.Probe()
-	log.Println(<-c.Results())
-	c.Stop()
+	ProbeWait = 0
+
+	// status code 204, not detected
+	t.Run("not detected", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusNoContent)
+		}))
+		defer ts.Close()
+		c := NewCPD(NewConfig())
+		c.config.Host = ts.Listener.Addr().String()
+		c.Start()
+		c.Probe()
+		r := <-c.Results()
+		if r.Detected {
+			t.Error("should not be detected")
+		}
+		c.Stop()
+	})
+
+	// status code 302, detected
+	t.Run("detected", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "http://example.com", http.StatusFound)
+		}))
+		defer ts.Close()
+		c := NewCPD(NewConfig())
+		c.config.Host = ts.Listener.Addr().String()
+		c.Start()
+		c.Probe()
+		r := <-c.Results()
+		if !r.Detected {
+			t.Error("should be detected")
+		}
+		c.Stop()
+	})
 }
 
 // TestCPDResults tests Results of CPD
