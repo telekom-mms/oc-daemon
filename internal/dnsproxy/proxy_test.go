b959d2bdc568d43522d246b7668e490b5e015696
hwipl <33433250+hwipl@users.noreply.github.com>
2024-01-12 20:11:50 +0100

    WIP: update tests in package dnsproxy
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/dnsproxy/proxy_test.go
+++ internal/dnsproxy/proxy_test.go
@@ -1,7 +1,12 @@
 package dnsproxy
 
 import (
+	"net"
 	"testing"
+	"time"
+
+	"github.com/miekg/dns"
+	log "github.com/sirupsen/logrus"
 )
 
 // getTestConfig returns a config for testing
@@ -11,6 +16,66 @@ func getTestConfig() *Config {
 		ListenUDP: true,
 		ListenTCP: true,
 	}
+}
+
+// TODO: use dns client to query proxy and test handleRequest instead?
+type responseWriter struct{}
+
+func (r *responseWriter) LocalAddr() net.Addr       { return nil }
+func (r *responseWriter) RemoteAddr() net.Addr      { return nil }
+func (r *responseWriter) WriteMsg(*dns.Msg) error   { return nil }
+func (r *responseWriter) Write([]byte) (int, error) { return 0, nil }
+func (r *responseWriter) Close() error              { return nil }
+func (r *responseWriter) TsigStatus() error         { return nil }
+func (r *responseWriter) TsigTimersOnly(bool)       {}
+func (r *responseWriter) Hijack()                   {}
+
+func getTestDNSServer(handler dns.Handler) *dns.Server {
+	s := &dns.Server{
+		Addr: "127.0.0.1:4255",
+		Net:  "udp",
+	}
+	s.Handler = handler
+	go s.ListenAndServe()
+	return s
+}
+
+func TestHandleRequest(t *testing.T) {
+	p := NewProxy(getTestConfig())
+
+	// without question in request
+	p.handleRequest(nil, &dns.Msg{})
+
+	// without remotes in proxy
+	p.handleRequest(nil, &dns.Msg{Question: []dns.Question{{Name: "test"}}})
+
+	// start remote
+	s := getTestDNSServer(dns.HandlerFunc(func(w dns.ResponseWriter, r *dns.Msg) {
+		reply := &dns.Msg{}
+		reply.SetReply(r)
+		cname, _ := dns.NewRR("test.example.com 3600 in CNAME example.com.")
+		a, _ := dns.NewRR("example.com. 3600 IN A 127.0.0.1")
+		aaaa, _ := dns.NewRR("example.com. 3600 IN AAAA ::1")
+		reply.Answer = []dns.RR{cname, aaaa, a}
+		if err := w.WriteMsg(reply); err != nil {
+			log.WithError(err).Error("error sending reply")
+		}
+	}))
+	defer s.Shutdown()
+	time.Sleep(time.Second)
+
+	// with remotes in proxy
+	p.SetRemotes(map[string][]string{".": {s.Addr}})
+	p.handleRequest(&responseWriter{}, &dns.Msg{Question: []dns.Question{{Name: "test.example.com."}}})
+
+	// with watches in proxy
+	p.SetWatches([]string{"test.example.com."})
+	go func() {
+		for r := range p.Reports() {
+			r.Done()
+		}
+	}()
+	p.handleRequest(&responseWriter{}, &dns.Msg{Question: []dns.Question{{Name: "test.example.com."}}})
 }
 
 // TestProxyStartStop tests Start and Stop of Proxy
@@ -60,6 +125,7 @@ func TestNewProxy(t *testing.T) {
 		p.watches == nil ||
 		p.reports == nil ||
 		p.done == nil ||
+		p.closed == nil ||
 		p.stopClean == nil ||
 		p.doneClean == nil {
 
