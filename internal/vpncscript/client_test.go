ed082df354bc6cce2720179ac56539a641c4b166
hwipl <33433250+hwipl@users.noreply.github.com>
2024-01-29 14:15:43 +0100

    WIP: update tests in package vpncscript
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/vpncscript/client_test.go
+++ internal/vpncscript/client_test.go
@@ -1,6 +1,39 @@
 package vpncscript
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
 
+	"github.com/telekom-mms/oc-daemon/internal/api"
+	"github.com/telekom-mms/oc-daemon/internal/daemon"
+)
+
+// TestRunClient tests runClient.
 func TestRunClient(t *testing.T) {
+	sockfile := filepath.Join(t.TempDir(), "sockfile")
+	config := api.NewConfig()
+	config.SocketFile = sockfile
+
+	// without errors
+	server := api.NewServer(config)
+	go func() {
+		for r := range server.Requests() {
+			r.Close()
+		}
+	}()
+	server.Start()
+	runClient(sockfile, &daemon.VPNConfigUpdate{})
+	server.Stop()
+
+	// with error reply
+	server = api.NewServer(config)
+	go func() {
+		for r := range server.Requests() {
+			r.Error("test error")
+			r.Close()
+		}
+	}()
+	server.Start()
+	runClient(sockfile, &daemon.VPNConfigUpdate{})
+	server.Stop()
 }
