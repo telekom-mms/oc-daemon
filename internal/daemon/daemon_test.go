523ac1ae8052d02a3bc376af3bcaf7af1560b23d
hwipl <33433250+hwipl@users.noreply.github.com>
2024-02-01 18:04:28 +0100

    WIP: update tests in package daemon
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/daemon/daemon_test.go
+++ internal/daemon/daemon_test.go
@@ -6,13 +6,70 @@ import (
 	"testing"
 
 	"github.com/telekom-mms/oc-daemon/internal/api"
+	"github.com/telekom-mms/oc-daemon/internal/dbusapi"
+	"github.com/telekom-mms/oc-daemon/pkg/vpnstatus"
 )
 
 func TestDaemonHandleClientRequest(t *testing.T) {
-	d := &Daemon{}
+	d := &Daemon{
+		status: vpnstatus.New(),
+		token:  "token",
+
+		dbus: dbusapi.NewService(),
+	}
+
+	// empty update
 	c1, c2 := net.Pipe()
+
 	r := api.NewRequest(c1, api.NewMessage(api.TypeVPNConfigUpdate, []byte{}))
 	go d.handleClientRequest(r)
+
+	c2.Close()
+
+	// invalid update
+	c1, c2 = net.Pipe()
+
+	u := NewVPNConfigUpdate()
+	b, err := u.JSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r = api.NewRequest(c1, api.NewMessage(api.TypeVPNConfigUpdate, b))
+	go d.handleClientRequest(r)
+
+	c2.Close()
+
+	// disconnect, wrong token
+	c1, c2 = net.Pipe()
+
+	u.Reason = "disconnect"
+	u.Token = "wrong"
+
+	b, err = u.JSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r = api.NewRequest(c1, api.NewMessage(api.TypeVPNConfigUpdate, b))
+	go d.handleClientRequest(r)
+
+	c2.Close()
+
+	// disconnect, with right token
+	c1, c2 = net.Pipe()
+
+	u.Token = d.token
+
+	b, err = u.JSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r = api.NewRequest(c1, api.NewMessage(api.TypeVPNConfigUpdate, b))
+	// TODO: wait for goroutine to finish before closing the pipe?
+	go d.handleClientRequest(r)
+
 	c2.Close()
 }
 
