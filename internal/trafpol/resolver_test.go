75c3bd61d1bd3d8622ccda8b6ac2c574ad3607cc
hwipl <33433250+hwipl@users.noreply.github.com>
2024-06-25 12:10:53 +0200

    Rework name resolution for allowed hosts in TrafPol
    Rework the internal name resolution for allowed hosts and handling of
    resolved names in TrafPol:
    
    - Split allowed hosts into allowed addresses and allowed names
    - Add resolver for allowed names
    - Use resolved names and allowed addresses for setting filter rules
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/trafpol/resolver_test.go
@@ -0,0 +1,92 @@
+package trafpol
+
+import (
+	"net"
+	"testing"
+	"time"
+)
+
+// TestResolverStartStop tests Start and Stop of Resolver.
+func TestResolverStartStop(_ *testing.T) {
+	config := NewConfig()
+	names := []string{}
+	updates := make(chan *ResolvedName)
+	r := NewResolver(config, names, updates)
+	r.Start()
+	r.Stop()
+}
+
+// TestResolverResolve tests Resolve of Resolver.
+func TestResolverResolve(_ *testing.T) {
+	config := NewConfig()
+	config.ResolveTriesSleep = 0
+	config.ResolveTimer = 0
+	names := []string{"does not exist...", "example.com"}
+	updates := make(chan *ResolvedName)
+	r := NewResolver(config, names, updates)
+
+	r.Start()
+
+	// test resolve
+	r.Resolve()
+	u := <-updates // use it for test with changes
+
+	// test double update, should not resolve entries because of TTL
+	r.Resolve()
+	r.Resolve()
+
+	// wait for timer
+	time.Sleep(time.Second)
+
+	r.Stop()
+
+	// try to test resolve with changes, using previous update
+	if len(u.IPs) > 0 {
+		names = []string{"example.com"}
+		r = NewResolver(config, names, updates)
+		r.names["example.com"] = u
+		r.names["example.com"].IPs[0] = net.ParseIP("127.0.0.1")
+
+		r.Start()
+
+		r.Resolve()
+		<-updates
+
+		r.Stop()
+	}
+}
+
+// TestNewResolver tests NewResolver.
+func TestNewResolver(t *testing.T) {
+	config := NewConfig()
+	names := []string{"test.example.com"}
+	updates := make(chan *ResolvedName)
+	r := NewResolver(config, names, updates)
+
+	// check resolver
+	if r == nil ||
+		r.config != config ||
+		r.updates != updates ||
+		r.cmds == nil ||
+		r.done == nil ||
+		r.closed == nil {
+
+		t.Fatalf("invalid resolver")
+	}
+
+	// check names
+	if len(r.names) != len(names) {
+		t.Fatalf("names do not match")
+	}
+	for rn := range r.names {
+		found := false
+		for _, n := range names {
+			if n == rn {
+				found = true
+			}
+		}
+		if !found {
+			t.Fatalf("names do not match")
+		}
+	}
+}
