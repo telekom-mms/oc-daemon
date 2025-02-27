ba6e50129c3edd62b9bf4920c932569507e38414
hwipl <33433250+hwipl@users.noreply.github.com>
2025-02-27 17:12:33 +0100

    WIP: update
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- test/ocserv/ocserv-docker-run.sh
+++ test/ocserv/ocserv-docker-run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run --rm --mount type=bind,src=.,dst=/ocserv -P oc-daemon-test-ocserv
+docker run \
+	--rm \
+	--mount type=bind,src=.,dst=/ocserv \
+	-P \
+	--cap-add NET_ADMIN \
+	--network oc-daemon-test \
+	--name ocserv \
+	oc-daemon-test-ocserv
