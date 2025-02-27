5c64e0ce5659901a7c927db6b1bab6a1ec88c219
hwipl <33433250+hwipl@users.noreply.github.com>
2025-02-27 17:12:33 +0100

    WIP: update
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- test/ocserv/deb12-docker-run.sh
+++ test/ocserv/deb12-docker-run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd ../..
-docker run --rm --mount type=bind,src=.,dst=/oc-daemon oc-daemon-test-deb12
+docker run \
+	--rm \
+	--mount type=bind,src=.,dst=/oc-daemon \
+	--cap-add NET_ADMIN \
+	--network oc-daemon-test \
+	--name deb12 \
+	oc-daemon-test-deb12
--- test/ocserv/deb12.sh
+++ test/ocserv/deb12.sh
@@ -3,8 +3,15 @@ set -e
 
 # install oc-daemon
 apt update
-apt install -y procps systemd-resolved
+apt install -y procps systemd-resolved gnutls-bin
 apt install -y /oc-daemon/dist/*.deb
+
+# create client key, client certificate
+certtool --generate-privkey --outfile /key.pem
+certtool --generate-self-signed \
+	--load-privkey /key.pem \
+	--template /oc-daemon/test/ocserv/client.tmpl \
+	--outfile /cert.pem
 
 # create tun device
 mkdir -p /dev/net
